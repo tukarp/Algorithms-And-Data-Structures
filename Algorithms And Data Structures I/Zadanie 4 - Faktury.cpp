f2bb261d3ac2d2c1a3967f1b9d64b3ed99db4069
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-02-22 14:56:59 +0100

    Update Zadanie 4 - Faktury.cpp

--- Algorithms And Data Structures I/Zadanie 4 - Faktury.cpp
+++ Algorithms And Data Structures I/Zadanie 4 - Faktury.cpp
@@ -3,74 +3,91 @@
 
 using namespace std;
 
-// lisc w drzewie binarnym
+// pojedynczy element drzewa
 struct Node {
-    string data;
+    string value;
     Node * left;
     Node * right;
     int counter = 0;
-
-    Node(string data) {
-        this -> data = data;
+    Node(string value) {
+        this -> value = value;
         left = NULL;
         right = NULL;
     }
 };
 
-// drzewo binarne
+// klasa drzewa binarnego
 class Binary_tree {
 private:
     Node * root;
-
 public:
-
-    // Utworzenie drzewa
+    // konstruktor
     Binary_tree() {
         root = NULL;
     }
 
-    void insert(string new_data) {
+    // destruktor
+    ~Binary_tree() {
+        delete root;
+    }
+
+    // dodawanie elementu do drzewa
+    void insert(string new_value) {
+        // jeśli drzewo jest puste dodaj element zamiast korzenia
         if(root == NULL) {
-            root = new Node(new_data);
+            root = new Node(new_value);
+            // w przeciwnym przypadku wywołaj funkcję od korzenia
         } else {
-            insert(new_data, root);
+            insert(new_value, root);
         }
     }
 
-    // wprowadzanie danych do drzewa
-    void insert(string new_data, Node * leaf) {
-        if(new_data < leaf -> data) {
-            if(leaf -> left != NULL) {
-                insert(new_data, leaf -> left);
+    // dodawanie elementu do drzewa
+    void insert(string new_value, Node * leaf) {
+        // jeśli nowa wartość jest mniejsza od liścia
+        if(new_value < leaf->value) {
+            // jeśli lewy liść nie jest pusty
+            if(leaf->left != NULL) {
+                // wywołaj insert() od lewego liścia
+                insert(new_value, leaf->left);
+                // w przeciwnym przypadku dodaj wartość do lewego liścia
             } else {
-                leaf -> left = new Node(new_data);
+                leaf->left = new Node(new_value);
             }
-        } else if(new_data > leaf -> data) {
-            if(leaf -> right != NULL) {
-                insert(new_data, leaf -> right);
+            // jeśli nowa wartość jest większa od liścia
+        } else if(new_value > leaf->value) {
+            // jeśli prawy liść nie jest pusty
+            if(leaf->right != NULL) {
+                // wywołaj insert() od prawego liścia
+                insert(new_value, leaf->right);
+                // w przeciwnym przypadku dodaj wartość do prawego liścia
             } else {
-                leaf -> right = new Node(new_data);
+                leaf->right = new Node(new_value);
             }
+            // jeśli wartość jest taka sama to inkrementuj licznik dla danego liścia
         } else {
-            leaf -> counter++;
+            leaf->counter++;
         }
     }
 
+    // zacznij wyświetlać zawartość drzewa
     void print() {
         print(root);
         cout << '\n';
     }
 
-    // wyswietlenie drzewa od najmniejszego elementu do najwiekszego
+    // wyświetl zawartość drzewa
     void print(Node * leaf) {
+        // jeśli liść nie jest zerem idź dalej
         if(leaf != NULL) {
-            print(leaf -> left);
-
-            for(int i = 0; i < leaf -> counter + 1; i++) {
-                cout << leaf -> data << '\n';
+            // najpierw wyświetl lewy liść
+            print(leaf->left);
+            // potem wyświetl wartość elementu tyle razy ile ma zapisane w zmiennej counter
+            for(int i = 0; i < leaf->counter + 1; i++) {
+                cout << leaf->value << '\n';
             }
-
-            print(leaf -> right);
+            // na końcu wyświetl prawy liść
+            print(leaf->right);
         }
     }
 };
@@ -80,24 +97,24 @@ int main() {
     std::cout.tie(nullptr);
     std::cin.tie(nullptr);
 
-    unsigned int n; // n - ilosc faktur, zakres <0, 3000000>
-    Binary_tree * drzewo_binarne = new Binary_tree(); // utowrzenie drzewa binarnego
+    unsigned int n;             // ilość faktur - n = <0, 3000000>
 
-    // pobranie ilosci faktur
+    // wprowadzenie ilości faktur
     cin >> n;
 
-    // wprowadzanie zmiennych do drzewa
+    // wprowadzenie danych do drzewa
+    Binary_tree * binary_tree = new Binary_tree();
+    string input;
     for(int i = 0; i < n; i++) {
-        string temp;
-        cin >> temp;
-        drzewo_binarne -> insert(temp);
+        cin >> input;
+        binary_tree->insert(input);
     }
 
-    // wyswietlenie danych
-    drzewo_binarne -> print();
+    // wyświetlenie zawartości drzewa
+    binary_tree->print();
 
-    // usuniecie drzewa
-    delete drzewo_binarne;
+    // zwolnienie pamięci
+    binary_tree->~Binary_tree();
 
     return 0;
 }
